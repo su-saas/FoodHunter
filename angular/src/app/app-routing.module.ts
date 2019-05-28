df68da4010725c38ca114398a740c28b9e4a539f
helena77 <lena@symphoni.cc>
2019-05-27 17:07:53 -0700

    added new component path to routes

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -4,12 +4,22 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { RestaurantComponent } from './restaurant/restaurant.component';
 import { AddReviewComponent } from './review/addReview/addReview.component';
+import { SearchComponent } from './search/search.component';
+import { FilterComponent } from './filter/filter.component';
+import { TagComponent } from './tag/tag.component';
+import { RecommendationListComponent } from './recommendation-list/recommendation-list.component';
+import { AlgorithmComponent } from './algorithm/algorithm.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   {path: 'restaurants/:rID', component: RestaurantComponent},
-  {path: 'addreview', component: AddReviewComponent}
+  {path: 'addreview', component: AddReviewComponent},
+  {path: 'search', component: SearchComponent},
+  {path: 'filter', component: FilterComponent},
+  {path: 'tag', component: TagComponent},
+  {path: 'recommendation-list', component: RecommendationListComponent},
+  {path: 'algorithm', component: AlgorithmComponent}
 ];
 
 @NgModule({
