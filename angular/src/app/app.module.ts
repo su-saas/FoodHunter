b4e5cab2407a622ac1fb1b2e362a21e7133c1fce
helena77 <helenawang77@gmail.com>
2019-05-28 21:02:37 -0700

    merge

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -19,7 +19,6 @@ import { AddReviewComponent } from './review/addReview/addReview.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ProfileService } from './profile.service';
 import { NotFoundComponent } from './not-found/not-found.component';
-<<<<<<< HEAD
 import { SearchComponent } from './search/search.component';
 import { FilterComponent } from './filter/filter.component';
 import { RecommendationListComponent } from './recommendation-list/recommendation-list.component';
@@ -29,12 +28,6 @@ import { TagSelectionComponent } from './tag-selection/tag-selection.component';
 @NgModule({
   declarations: [
     AppComponent,
-=======
-
-@NgModule({
-	declarations: [
-		AppComponent,
->>>>>>> master
 		LoginComponent,
 		RegisterComponent,
 		CollectionComponent,
@@ -42,15 +35,11 @@ import { TagSelectionComponent } from './tag-selection/tag-selection.component';
 		ReviewComponent,
 		AddReviewComponent,
 		ProfileComponent,
-<<<<<<< HEAD
 		NotFoundComponent,
     SearchComponent,
     FilterComponent,
     RecommendationListComponent,
     TagSelectionComponent,
-=======
-		NotFoundComponent
->>>>>>> master
   ],
   imports: [
     BrowserModule,
