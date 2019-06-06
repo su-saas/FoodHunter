179b59a6814bc20706705d401583af74bc2e7e03
helena77 <helenawang77@gmail.com>
2019-06-06 00:33:48 -0700

    finished search

--- angular/src/app/search/search.component.ts
+++ angular/src/app/search/search.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
-import { SearchService } from '../search.service';
 import { IRestaurantModel } from '../interfaces/IRestaurantModel';
+import { SearchService } from '../services/search.service';
 
 @Component({
 	selector: 'app-search',
@@ -13,7 +13,8 @@ export class SearchComponent implements OnInit {
 	private nextStationUrl: string;
 	private restaurant: IRestaurantModel;
 	private rID: number;
-
+	private notFind: boolean;
+	private submitted: boolean;
 	options = [
 		{ description: 'Restaurant Name' },
 	];
@@ -22,19 +23,25 @@ export class SearchComponent implements OnInit {
 		private searchService: SearchService) { }
 
 	ngOnInit() {
-
+		this.notFind = true;
+		this.submitted = false;
 	}
 
 	onSubmit(f) {
+		this.submitted = true;
 		var name = String(f.value.restaurantName);
-		this.searchService.getRestaurantByName(name.toLowerCase())
+		console.log(name);
+		this.searchService.getRestaurantByName(name)
 			.subscribe(res => {
-				this.restaurant = res[0];
-				console.log(res);
-				this.rID = this.restaurant.restaurantID;
-				console.log(this.rID);
-				this.nextStationUrl = this.restaurantDetailUrl + this.rID;
-				this.router.navigateByUrl(this.nextStationUrl);
+				if (res.length > 0) {
+					this.notFind = false;
+					this.restaurant = res[0];
+					console.log(res);
+					this.rID = this.restaurant.restaurantID;
+					console.log(this.rID);
+					this.nextStationUrl = this.restaurantDetailUrl + this.rID;
+					this.router.navigateByUrl(this.nextStationUrl);
+				}
 			});
-	}
+		}
 }
