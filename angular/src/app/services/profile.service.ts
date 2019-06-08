aa62cc3f8c499dd6e588335b61492797554a3aa1
helena77 <helenawang77@gmail.com>
2019-06-08 16:55:14 -0700

    updated profile

--- angular/src/app/services/profile.service.ts
+++ angular/src/app/services/profile.service.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { IUserModel } from '../interfaces/IUserModel';
 import { IFoodieModel } from '../interfaces/IFoodieModel';
 import { IFavoriteListModel } from '../interfaces/IFavoriteListModel';
+import { IFoodieTagListModel } from '../interfaces/IFoodieTagListModel';
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +13,16 @@ import { IFavoriteListModel } from '../interfaces/IFavoriteListModel';
 export class ProfileService {
   private foodieurl = 'http://localhost:8080/foodie/';
 // private url: string = 'https://foodhunter.azurewebsites.net/foodie/';
+  private tagListUrl = 'http://localhost:8080/tagList/';
   constructor(private http: HttpClient) { }
 
   getProfileByFoodieID(userID: number): Observable<IFoodieModel> {
     console.log(userID);
     return this.http.get<IFoodieModel>(this.foodieurl + userID);
+  }
+
+  getFoodieTagListByFoodieID(userID: number): Observable<IFoodieTagListModel> {
+    return this.http.get<IFoodieTagListModel>(this.tagListUrl + userID);
   }
 }
 
