68db2de197f8ca5722df0528c95e5468235b63f1
xxx0624 <xzheng0624@gmail.com>
2019-06-10 15:16:32 -0700

    compile again

--- googleOauth2.js
+++ googleOauth2.js
@@ -4,8 +4,11 @@ var googleOauth2 = /** @class */ (function () {
     function googleOauth2() {
     }
     // erica
-    googleOauth2.id = '203527927906-pki6dt4e7ko22rv7uga67l49r707dg08.apps.googleusercontent.com';
-    googleOauth2.secret = 'gFHBpRPcy5ciciOpJcKOV9J4';
+    //static id: string = '203527927906-pki6dt4e7ko22rv7uga67l49r707dg08.apps.googleusercontent.com';
+    //static secret: string = 'gFHBpRPcy5ciciOpJcKOV9J4';
+    // xing
+    googleOauth2.id = '545053256990-52m47d5kn2gst9ic1v21drlpqiduftie.apps.googleusercontent.com';
+    googleOauth2.secret = 'BqW8b2JcLzSmjmRmzs30DLge';
     return googleOauth2;
 }());
 exports["default"] = googleOauth2;
