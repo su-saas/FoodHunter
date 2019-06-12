ac23c6bef4cb1f2decfc3bcc9f37d4fa565851c8
Erica Chen <chenm17@seattleu.edu>
2019-06-11 21:33:47 -0700

    update tag selection reload

--- angular/src/app/profile/profile.component.ts
+++ angular/src/app/profile/profile.component.ts
@@ -1,17 +1,15 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { first } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
 import { ProfileService } from '../services/profile.service';
 import { AuthService } from '../services/auth.service';
 import { IFavoriteListModel } from '../interfaces/IFavoriteListModel';
-import { TabId } from '../utils/enum';
+
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements OnInit, OnDestroy {
+export class ProfileComponent implements OnInit {
   userID: number;
   userName: string;
   emailAddress: string;
@@ -19,40 +17,21 @@ export class ProfileComponent implements OnInit, OnDestroy {
   favoriateList: IFavoriteListModel;
   restaurantIDList: number[] = [];
   avatarPicture: string;
-  tabId = TabId;
 
-  // This is assuming that we always start with the current tag pri tab
-  currentTabSubject: BehaviorSubject<TabId> = new BehaviorSubject<TabId>(TabId.currentTagPriorityTab);
-
-  constructor(
-    private auth: AuthService,
-    private profileService: ProfileService
-  ) { }
+  constructor(private auth: AuthService,
+    private profileService: ProfileService) { }
 
   ngOnInit() {
-    this.auth.getSession().pipe(
-      first()
-    ).subscribe(data => {
+    this.auth.getSession().subscribe(data => {
       this.userID = data.userID;
       this.emailAddress = data.emailAddress;
       this.userName = data.userName;
       console.log('profile in profile: ' + JSON.stringify(data));
       console.log('user of profile: ', this.userID);
-      if (this.userID > 0) {
-        this.profileService.getProfileByFoodieID(this.userID).subscribe(
-          foodieinfo => this.avatarPicture = foodieinfo.avatar
-        );
-      }
+      this.profileService.getProfileByFoodieID(this.userID).subscribe(
+        foodieinfo => this.avatarPicture = foodieinfo.avatar
+      );
     });
-  }
-
-  ngOnDestroy(): void {
-    // Release subject subscription
-    this.currentTabSubject.complete();
-  }
-
-  onTabChage(currentTab: TabId): void {
-    this.currentTabSubject.next(currentTab);
   }
 
 }
